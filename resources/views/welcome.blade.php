36c216da03e8f1068969a11fe2a04dd87fff97d1
ndarupurnomo <ndaru.purnomo@gmail.com>
2016-02-22 16:17:42 +0700

    Started work on kulonuwun

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -39,6 +39,7 @@
         <div class="container">
             <div class="content">
                 <div class="title">Laravel 5.2, actually, really</div>
+                <div class="title">Kulonuwun...</div>
             </div>
         </div>
     </body>
